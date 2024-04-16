4743052bdcf3f08d0e0c89b873186f5f75b0c1b8
kalamanzy <Kpguinto2722@gmail.com>
2024-04-16 21:27:17 +0800

    tapos na

---
+++ admin/update-item.php
@@ -0,0 +1,88 @@
+<?php require_once('header-admin.php')?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../admin-css/reset.css">
+  <title>Adding Item</title>
+</head>
+<style>
+  :root {
+    --header-color: #124076;
+    --sidebar-color: #5F5C5C;
+    --sidebar-option-color: #444040;
+    --approve-color: #39BF17;
+    --reject-color: #FF0000;
+    --bg-color: #B5C0D0;
+    --first-row-color: #F2F2F2;
+    --sec-row-color: #ECF9FF;
+    --update-color: #F0E076;
+  }
+
+  .container {
+    height: calc(100vh - 60px);
+    background-color: var(--bg-color);
+
+    display: grid;
+    grid-template-rows: 80px 1fr;
+  }
+
+  .top {
+    margin: 40px auto;
+    font-size: 24px;
+    font-weight: 700;
+  }
+  
+  .form {
+    margin: 0 auto;
+    background-color: #ECF9FF;
+    padding: 35px 45px;
+    height: 235px;
+    border-radius: 2px;
+  }
+
+  label {
+    font-size: 14px;
+  }
+
+  input {
+    height: 30px;
+    width: 250px;
+    outline: none;
+    margin-bottom: 12px;
+  }
+
+  button {
+    border-radius: 2px;
+    border: none;
+    background-color: var(--header-color);
+    color: #ECF9FF;
+    font-weight: 500;
+    width: 100%;
+    height: 35px;
+    cursor: pointer;
+    transition: all .15s;
+  }
+
+  button:hover {
+    opacity: .9;
+  }
+</style>
+<body>
+  <div class="container">
+    <div class="top">Update Item</div>
+    <div class="form">
+      <form action="">
+        <label for="itemName">Item name</label><br>
+        <input type="text" id="itemName"><br>
+        <label for="quan">Quantity</label><br>
+        <input type="text" id="quan"><br>
+        <label for="cate">Category</label><br>
+        <input type="text" id="cate"><br>
+        <button>Update</button>
+    </form>
+    </div>
+  </div>
+</body>
+</html>
---
+++ student/signup-acc.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inventory System</title>
+
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet"/>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet"/>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.2.0/mdb.min.css" rel="stylesheet"/>
+       
+    <style>
+      body , html {
+        height: 100%;
+        margin: 0;
+        font-family: 'Roboto', sans-serif;
+      }
+
+      body {
+        background-image: url('../includes/images/dhvsu.png');
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+      }
+
+      .container-h1 {
+       position: absolute;
+       top: 20%;
+       left: 50%;
+       transform: translateX(-50%);
+       z-index: 3;  
+       color: white; 
+      }
+
+      .sign-up {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        z-index: 2;
+      }
+
+      .form-container {
+        background-color: rgba(255, 255, 255, 0.9); 
+        padding: 20px;
+        border-radius: 8px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        width: 80%;
+        max-width: 400px;
+      }
+
+      .form-container h2 {
+        text-align: center;
+        margin-bottom: 20px;
+      }
+
+      .form-container button,
+      .form-container a.btn {
+        display: block;
+        margin: 0 auto; 
+        text-align: center; 
+      }
+
+      .image-overlay {
+       position: absolute;
+       top: 2%;
+       right: 50%;
+       z-index: 1; 
+       filter: blur(1px);
+      }
+
+      .image-overlay img {
+       width: 1000px;  
+       height: auto; 
+       filter: blur(1px); 
+      }
+
+    </style>
+</head>
+<body>
+<div class="container-h1">
+   <h1>Inventory System</h1>
+</div>
+<div class="sign-up">
+    <div class="form-container">
+        <h2>SIGN UP</h2>
+        <form>
+            <div class="mb-3">
+                <label for="username" class="form-label">Username</label>
+                <input type="text" class="form-control" id="username" name="username" required>
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password" name="password" required>
+                
+            </div>
+            <div class="mb-3">
+                <label for="confim-password" class="form-label">Confirm password</label>
+                <input type="password" class="form-control" id="confirm-password" name="confirm-password" required>
+            </div>
+            <a href="dashboard.php" class="btn btn-primary">Log In</a>
+            <div class="mb-2 text-center">
+                <p>Don't have an account? <a href="signup.php">Sign up</a></p>
+            </div>
+        </form>
+    </div>
+</div>
+
+<div class="image-overlay">
+    <img src="../includes/images/ccs1.png" alt="ccs.png">
+</div>
+
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.2.0/mdb.umd.min.js"></script>
+</body>
+</html>
